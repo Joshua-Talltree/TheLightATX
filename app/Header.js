ab6bc2e36edd3bf1d31554dca297cb3965fc250c
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-11-04 19:56:08 -0500

    first commit

---
+++ app/Header.js
@@ -0,0 +1,11 @@
+import Header from './Header';
+
+class App extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Header message="Playing from Charts" />
+            </View>
+        );
+    }
+}
